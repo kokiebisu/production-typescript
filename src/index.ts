26b5991031ba9d43ca5fe345de64906537aaed02
Kenichi Okiebisu <a01056715@gmail.com>
2021-05-09 11:03:07 -0700

    added docs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+/**
+ * Calculates the sum of two numbers
+ * @param a - First number
+ * @param b - Second number
+ *
+ * @public
+ */
 export function sum2(a: number, b: number): number {
   const sum = a + b;
   return sum;
 }
+
+export { subtract } from './subtract';
---
+++ src/subtract.ts
@@ -0,0 +1,10 @@
+/**
+ * Calculates the difference of two numbers
+ * @param a - First number
+ * @param b - Second number
+ *
+ * @beta
+ */
+export const subtract = (a: number, b: number): number => {
+  return a - b;
+};
